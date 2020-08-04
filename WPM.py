ec9ac169bba0eb718a603f462283c945bed678c1
Mihir Rabade <mihirrabade@gmail.com>
2020-08-05 00:36:39 +0530

    Various Tkinter changes Tried various ways of bringing a window into fulscreen.

--- WPM.py
+++ WPM.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from time import sleep
-import tkinter as ttk
+import tkinter as tk
 import os
 
 
@@ -63,11 +63,38 @@ class configuration():
 
 
 # This code snippet now flashes the words per minute.
+"""
+class FullScreenApp(object):
+    def __init__(self, master, **kwargs):
+        self.master = master
+        pad = 3
+        self._geom = '200x200+0+0'
+        master.geometry("{0}x{1}+0+0".format(
+            master.winfo_screenwidth() - pad, master.winfo_screenheight() - pad))
+        master.bind('<Escape>', self.toggle_geom)
+
+    def toggle_geom(self, event):
+        geom = self.master.winfo_geometry()
+        print(geom, self._geom)
+        self.master.geometry(self._geom)
+        self._geom = geom
+"""
 
 cofg = configuration()
 para = open("para.txt", "r")
 words = para.read()
-top = ttk.Tk()
-
+root = tk.Tk()
+root.attributes("-fullscreen", True)
+root.bind("<F11>", lambda event: root.attributes("-fullscreen",
+                                                 not root.attributes("-fullscreen")))
+root.bind("<Escape>", lambda event: root.attributes("-fullscreen", False))
+"""
+top.geometry()
 top.attributes("-fullscreen", True)
 top.bind('<Escape>', top.attributes("-fullscreen", False))
+"""
+# WPMFlash = FullScreenApp(top)
+WPMFlash = tk.Frame(root)
+WPMFlash.pack()
+
+root.mainloop()
