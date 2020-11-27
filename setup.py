b14c886d246b8e52080c10a743c40e9620e370d4
Mihir Rabade <mihirrabade@gmail.com>
2020-11-27 08:06:56 +0530

    Removed Cxfreeze setup config

--- setup.py
+++
@@ -1,6 +0,0 @@
-from cx_Freeze import Executable, setup
-
-setup(name="Words Per Minute!",
-      version="1.0.1",
-      description="This program lets you display a paragraph, word by word; in full screen!",
-      executables=[Executable("WPM.py")])
